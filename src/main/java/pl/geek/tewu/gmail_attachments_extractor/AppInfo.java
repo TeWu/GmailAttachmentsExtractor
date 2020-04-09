be10034ac0c7c6dbfee491115459080e8f25349d
Tomasz Więch <tewu.dev@gmail.com>
2020-04-09 15:01:57 +0200

    Add AppInfo

---
+++ src/main/java/pl/geek/tewu/gmail_attachments_extractor/AppInfo.java
@@ -0,0 +1,8 @@
+package pl.geek.tewu.gmail_attachments_extractor;
+
+public class AppInfo {
+    public static final String NAME = "Gmail Attachments Extractor";
+    public static final String SHORT_DESCRIPTION = "Downloads attachments from Gmail emails, then creates copy of emails but without attachments";
+    public static final String VERSION = "0.0.2";
+    public static final String COMMAND = "java -jar GmailAttachmentExtractor.jar";
+}
